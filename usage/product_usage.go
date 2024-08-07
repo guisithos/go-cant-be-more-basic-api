c266c0d0a98e3fc002de91910cb108a9ae1d41c1
Guilherme <gui@H-TINFNOT-3.unimednoroeste.intranet>
2024-08-07 20:11:56 -0300

    Being basic as possible, only one commit needed

---
+++ usage/product_usage.go
@@ -0,0 +1,42 @@
+package usage
+
+import (
+	"go-cant-be-more-basic-api/model"
+	repository "go-cant-be-more-basic-api/repo"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+
+	productId, err := pu.repository.CreateProduct(product)
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+
+	return product, nil
+}
+
+func (pu *ProductUsecase) GetProductById(id_product int) (*model.Product, error) {
+
+	product, err := pu.repository.GetProductById(id_product)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
